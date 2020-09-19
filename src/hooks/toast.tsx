62de8c7d873342a06c53fc9a4b10ee0ddcd93113
Maicon Andrade <andrade.maicon.pr@gmail.com>
2020-09-19 20:26:24 -0300

    Toast hook created

---
+++ src/hooks/toast.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useCallback, useContext} from 'react';
+
+import ToastContainer from '../components/ToastContainer';
+
+
+interface ToastContextData {
+  addToast(): void;
+  removeToast(): void;
+}
+
+const ToastContext = createContext<ToastContextData>({} as ToastContextData);
+
+export const ToastProvider: React.FC = ({ children }) => {
+  const addToast = useCallback(() => {
+    console.log('Add Toast')
+  }, []);
+
+  const removeToast = useCallback(() => {
+    console.log('Remove Toast')
+  }, []);
+
+  return (
+    <ToastContext.Provider value={{
+      addToast, removeToast
+    }}>
+      {children}
+      <ToastContainer />
+    </ToastContext.Provider>
+  )
+}
+
+export function useToast(): ToastContextData {
+  const context = useContext(ToastContext);
+
+  if(!context) {
+    throw new Error('useToat must be used within a ToastProvider')
+  }
+
+  return context
+}
